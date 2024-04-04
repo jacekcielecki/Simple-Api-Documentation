239020aa08110b0ddb6a4ca01066b9e9aafcbf3e
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2024-04-04 18:40:41 +0200

    add UseApiDocs func

---
+++ source/SimpleApiDocumentation.Core/ApiDocsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace SimpleApiDocumentation.Core;
+
+public static class ApiDocsExtensions
+{
+    /// <summary>
+    /// Adds endpoint for Api documentation.
+    /// </summary>
+    public static IApplicationBuilder UseApiDocs(this IApplicationBuilder app)
+    {
+        ArgumentNullException.ThrowIfNull(app);
+        return app;
+    }
+}

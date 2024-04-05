4411ed509c8343476dd6cbcd780b520cff7df49c
Jacek Cielecki <93492850+Jackk83@users.noreply.github.com>
2024-04-05 19:47:11 +0200

    WebApi playground better endpoints for package testing & reference

---
+++ source/SimpleApiDocumentation.Test/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApiDocumentation.Test;
+
+[Route("api/[controller]")]
+[ApiController]
+[AllowAnonymous]
+public class ProductController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok("test");
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        return Ok("test");
+    }
+
+    [HttpPost]
+    public IActionResult Create()
+    {
+        return Ok("test");
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        return Ok("test");
+    }
+
+    [HttpPut("{id}/{newName}")]
+    public IActionResult Update(int id, string newName)
+    {
+        return Ok("test");
+    }
+}
--- source/SimpleApiDocumentation.Test/Program.cs
+++ source/SimpleApiDocumentation.Test/Program.cs
@@ -12,28 +12,11 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.MapGet("api/user", () => "test");
+app.MapDelete("api/user/{id}", (int id) => "test");
+app.MapPost("api/user", (string user) => "test");
+app.MapPut("api/user/{id}", () => "test");
 
 app.UseApiDocs();
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
